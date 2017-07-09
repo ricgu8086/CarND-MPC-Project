f09b5ad15e6dda85d8cc8cfce33971e0e05c3a48
ricgu8086 <ricgu8086@gmail.com>
2017-07-09 12:27:08 -0700

    Fixed bug: model equation was not updated for the simulator constraints (opposite psi).

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -155,7 +155,7 @@ class FG_eval
         fg[1 + psi_start + t] = psi1 - (psi0 - v0/Lf * delta0 * dt);
         fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
         fg[1 + cte_start + t] = cte1 - (f0 - y0 + v0 * CppAD::sin(epsi0) * dt);
-        fg[1 + epsi_start + t] = epsi1 - (psi0 - psi_des + v0/Lf * delta0 * dt);
+        fg[1 + epsi_start + t] = epsi1 - (psi0 - psi_des - v0/Lf * delta0 * dt);
       }
     }
 };
